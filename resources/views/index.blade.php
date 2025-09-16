1973ae726e6ac2778b6ba4cde02d8e931ee327cf
cortes <ejcortes.8562@unicesmag.edu.co>
2025-09-16 16:13:05 -0500

    botones, tabla, graficos, formulario y registrar datos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,22 @@
 @extends('layout.app')
 @section('title', 'Inicio')
 
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+<div class="container" style="margin-top: 20px; text-align: center;">
 
+  <!-- Título principal -->
+  <h1>Bienvenido a mi aplicación</h1>
+
+  <!-- Imagen -->
+  <img src="{{ asset('img/Internet_of_Things-1024x585 - copia.jpg') }}" 
+       alt="Imagen de ejemplo" 
+       style="width: 100%; max-width: 400px; margin: 20px 0;">
+
+  <!-- Botones -->
+  <div>
+    <a href="#" class="btn btn-primary">Tabla</a>
+    <a href="#" class="btn btn-success">Gráficas</a>
+    <a href="#" class="btn btn-warning">Formularios</a>
+    <a href="#" class="btn btn-danger">Datos del registrador</a>
+  </div>
+
+</div>
